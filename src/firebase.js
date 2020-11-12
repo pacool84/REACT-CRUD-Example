23bc971c858ca3cc8f79dff6488f4033ee63aaca
Francisco López <flopez_1384@hotmail.com>
2020-11-11 20:04:06 -0600

    Instalando create react app y agregando la configuracion de Firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB4ntIlgGOf9RSAUzfzCeo-sc1qCuz7aG4",
+  authDomain: "agenda-crud-e08ad.firebaseapp.com",
+  databaseURL: "https://agenda-crud-e08ad.firebaseio.com",
+  projectId: "agenda-crud-e08ad",
+  storageBucket: "agenda-crud-e08ad.appspot.com",
+  messagingSenderId: "12671917854",
+  appId: "1:12671917854:web:a48d1a7b6adde4b1f5330f",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
