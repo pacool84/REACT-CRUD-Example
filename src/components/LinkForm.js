a78508973d0f566e93075ba5a82644bb490799b4
Francisco López <flopez_1384@hotmail.com>
2020-11-20 16:48:47 -0600

    Dando mas estilo al componente LinkForm y agregando logica de Submit al boton

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 const LinkForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
-    <form className='card card-body'>
+    <form className='card card-body' onSubmit={handleSubmit}>
       <div className='form-group input-group'>
-        <div className='input-group-text bg-light'></div>
+        <div className='input-group-text bg-light'>
+          <i className='material-icons'>insert_link</i>
+        </div>
         <input
           type='text'
           className='form-control'
@@ -12,6 +19,26 @@ const LinkForm = () => {
           name='url'
         />
       </div>
+      <div className='form-group input-group'>
+        <div className='input-group-text bg-light'>
+          <i className='material-icons'>create</i>
+        </div>
+        <input
+          type='text'
+          className='form-control'
+          name='name'
+          placeholder='Website Name'
+        />
+      </div>
+      <div className='form-group'>
+        <textarea
+          name='description'
+          rows='3'
+          className='form-control'
+          placeholder='Write a description'
+        ></textarea>
+      </div>
+      <button className='btn btn-primary btn-block'>Save</button>
     </form>
   );
 };
