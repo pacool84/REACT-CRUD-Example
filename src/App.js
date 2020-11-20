98aaec854aad32054127a0b38658a9823ecd116f
Francisco López <flopez_1384@hotmail.com>
2020-11-20 08:33:59 -0600

    Instalando libreria bootswatch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import Links from "./components/Links";
+import LinkForm from "./components/LinkForm";
 
 function App() {
-  return <Links />;
+  return (
+    <>
+      <LinkForm />
+      <Links />;
+    </>
+  );
 }
 
 export default App;
